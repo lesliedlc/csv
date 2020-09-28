38cd4157014f7b9d7ca60d9adccb4a8ab78372da
LP <leslie_posadadelacr1@baylor.edu>
2020-09-27 23:39:09 -0500

    commits

--- sitka_valley_temps.py
+++ sitka_valley_temps.py
@@ -4,9 +4,7 @@ from datetime import datetime
 open_sitka = open("sitka_weather_2018_simple.csv", "r")
 open_valley = open("death_valley_2018_simple.csv", "r")
 
-#csv_sitka = csv.reader(open_sitka, delimiter=",")
-csv_valley = csv.reader(open_valley, delimiter=",")
-
+csv_valley = csv.DictReader(open_valley, delimiter=",")
 csv_sitka = csv.DictReader(open_sitka, delimiter=",")
 
 header_sitka = next(csv_sitka)
@@ -31,14 +29,14 @@ for row in csv_sitka:
 #information for death valley 
 for row in csv_valley:
     try:
-        highdv = int(row[4])
-        lowdv = int(row[5])
-        current_date = datetime.strptime(row[2], "%Y-%m-%d") #index of date, formats the date
+        highdv = int(row["TMAX"])
+        lowdv = int(row["TMIN"])
+        current_date = datetime.strptime(row["DATE"], "%Y-%m-%d") #index of date, formats the date
     except ValueError:
         print(f"Missing data for {current_date}")
     else:
-        low_v.append(int(row[5])) #index of tempmin
-        high_v.append(int(row[4]))
+        low_v.append(int(row["TMIN"])) #index of tempmin
+        high_v.append(int(row["TMAX"]))
         date_v.append(current_date)
 
 import matplotlib.pyplot as plt
@@ -53,11 +51,11 @@ ax[1].plot(date_v, low_v, c="blue", alpha = 0.5)
 ax[1].plot(date_v, high_v, c="red", alpha = 0.5)
 
 #sitka graph
-ax[0].set_title("SITKA AIRPORT, AK US", fontsize = 12)
+ax[0].set_title(header_sitka["NAME"], fontsize = 12)
 ax[0].tick_params(axis="both",labelsize = 12) 
 ax[0].fill_between(date_s, high_s, low_s, facecolor = "blue", alpha = 0.1)
 #deathvalley graph
-ax[1].set_title("DEATH VALLEY, CA US", fontsize = 12)
+ax[1].set_title(header_valley["NAME"], fontsize = 12)
 ax[1].tick_params(axis="both",labelsize = 12) 
 ax[1].fill_between(date_v, high_v, low_v, facecolor = "blue", alpha = 0.1)
 
